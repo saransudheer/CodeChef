dcdbcb8de591f2a1be3b7fe228f40439d700cdb0
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-10-03 09:46:29 +0530

    In progress

--- Contests/Cooktober/chef_query.cpp
+++ Contests/Cooktober/chef_query.cpp
@@ -1,28 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 int solve(int a[],int n,int k){
-	int rem=0, solved=0, day=0, count=1;
+	int rem=0,day=0, rum=0, current=0;
+	
 	for(int i=0; i<n; i++){
-		solved=abs(a[i]-k);
-		if(solved<k && rem>0){
-			rem=rem-solved;
-			++count;
-			break;
-		}else if(solved>=k && rem>0){
-			rem=rem+solved;
-			++count;
-			break;
-		}else if(solved>=k && rem==0){
-			rem=rem+solved;
-			++count;
-			break;			
-		}
-		else if(solved==k && rem==0){
-			day=count+1;
-		}
-		else if(solved<k && rem==0){
-			day=count;
-		}
+	    if(a[i]>k){
+	        day+=(a[i]/k)+1;
+	    }else if(a[i]-k>=0){
+	        current=(rem+a[i])-k;
+	        rem+=current;
+	        day=i;
+	    }else if(a[i]-k<0){
+	        if(rem>0){
+	            current=k-a[i];
+	            rem=rem-current;
+	            day=i;
+	            if(rem<0){
+	                day=i;
+	            }
+	        }else{
+	            day+=1;
+	        }
+	            
+	    }
+	    
 	}
 		
 	return day;
@@ -34,17 +35,16 @@ int main()
 	int t;
 	scanf("%d",&t);
 	int ans[t];
-	int n,k;
-	scanf("%d%d",&n,&k);
-	int a[n];
-	for(int i=0; i<n; i++){
-		scanf("%d",&a[i]);
+	while(t--){
+		int n,k;
+		scanf("%d%d",&n,&k);
+		int a[n];
+		for(int i=0; i<n; i++){
+			scanf("%d",&a[i]);
+		}
+		cout<<solve(a,n,k)<<endl;
 	}
-	ans[0]=solve(a,n,k);
 	
-	for(int i=0; i<t; i++){
-		cout<<ans[i]<<endl;
-	}
 	return 0;
 }
 		
